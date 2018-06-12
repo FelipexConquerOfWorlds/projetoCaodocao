2530d1f287a64f1fdd365aafce9a1507702a5193
FelipexConquerOfWorlds <felipepborba@hotmail.com>
2018-06-12 14:18:42 -0300

    Add files via upload

--- caodocao/models/Conexao.php
+++ caodocao/models/Conexao.php
@@ -1,35 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aluno
- * Date: 08/05/18
- * Time: 09:22
- */
 
 class Conexao
 {
         const HOST = "localhost";
         const DBNAME = "caodocao";
-        const USUARIO = "admin";
-        const SENHA = "admin";
+        const USUARIO = "root";
+        const SENHA = "root";
 
 
         public static $conexao = null;
 
 
         public static function getConexao(){
-            try{
-                if (self::$conexao == null){
-                    self::$conexao = new PDO("mysql:host=".self::HOST."dbname=".self::DBNAME, self::USUARIO, self::SENHA);
-                    self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    }
-
-                    return self::$conexao;
+            try{ $conexao = new PDO("mysql:host=".self::HOST.";dbname=".self::DBNAME.";", self::USUARIO, self::SENHA);
+            return $conexao;
+//                if (self::$conexao == null){
+//                    self::$conexao = new PDO("mysql:host=".self::HOST."dbname=".self::DBNAME, self::USUARIO, self::SENHA);
+//                    self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//                    }
+//
+//                    return self::$conexao;
 
             }catch (PDOException $e){
                     die("Falha na conexao:".$e->getMessage());
-
             }
-            return $conexao;
         }
 }
+
+$c = new Conexao();
+$c->getConexao();
