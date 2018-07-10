c43b929ac9a728f9c764c356bb399bd232f5a106
FelipexConquerOfWorlds <felipepborba@hotmail.com>
2018-07-10 14:27:09 -0300

    Add files via upload

---
+++ caodocao/banco/insertracas.sql
@@ -0,0 +1,83 @@
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (1 , 1 , 'Alaska Malamute');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (2, 1 , 'Basset Hound' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (3, 1 , 'Beagle');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (4, 1, 'Bichon');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (5, 1, 'Border Collie');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (6, 1 , 'Boston Terrier');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (7, 1, 'Boxer');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (8, 1, 'German Braco');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (9, 1, 'Bull Mastiff');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (10,1 , ' Bull Terrier');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (11,1 , 'Bulldog');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (12,1 ,'Bulldog Francês / French Bulldog' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (13,1 ,'Caniche / Poodle' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (14,1 , 'Cão de Água / Portuguese Water Dog');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (15,1 ,'Castro Laboreiro / Portuguese Castro Laboreiro' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (16,1 ,'Chihuahua' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (17,1 ,'Chow-Chow' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (18,1 ,'Cocker Spaniel' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (19,1 ,'Collie' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (20,1 , 'Dálmata / Dalmatian');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (21,1 , 'Doberman');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (22,1 , 'Dogue Argentino / Argentinian Dogue');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (23,1 , 'Dratar');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (24,1 ,'Epagneul Bretton' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (25,1 ,'Fila Brasileiro / Brazilian Fila' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (26,1 ,'Fox Terrier' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (27,1 ,'Galgo / Greyhound' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (28,1 ,'Golden Retriever' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (29,1 ,'Husky Siberiano / Siberian Husky' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (30,1 ,'Indefinida / Undefined' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (31,1 ,'Jack Russel Terrier' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (32,1 ,'Labrador Retriever' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (33,1 ,'Leão da Rodésia / Rhodesian ridgeback' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (34,1 ,'Mastim Napolitano / Napolitan Mastin' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (35,1 ,'Pastor Alemão / German Shepperd' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (36,1 , 'Pastor australiano / Australian Shepperd');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (37,1 ,'Pastor Belga / Belgian Shepperd' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (38,1 ,'Pekinois' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (39,1 ,'Perdigueiro Português / Portuguese Pointer' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (40,1 ,'Pinscher' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (41,1 , 'Pit Bull');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (42,1 ,'Podengo Português / Portuguese Podengo' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (43,1 ,'Pointer' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (44,1 ,'Rafeiro do Alentejo / Portuguese Rafeiro Alentejo' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (45,1 ,'Ratonero Andaluz' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (46,1 ,'Rottweiller' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (47,1 ,'Samoyedo' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (48,1 ,'Serra da Estrela / Portuguese Serra da Estrela' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (49,1 ,'Serra D Aires / Portuguese Serra D Aires' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (50,1 , 'Setter Irlandês / Irish Setter');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (51,1 ,'Springer Spaniel ' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (52,1 ,'Staffordshire Bull Terrier' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (53,1 ,'Staffordshire Terrier American' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (54,1 ,'Teckel' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (55,1 , 'Vizsla');
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (56,1 ,'Welsh Corgi' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (57,1 ,'West Highland Terrier' );
+INSERT INTO `raca` (`cod_raca`, `cod_especie`, `descricao`) VALUES (58,1 , 'Yorkshire Terrier');
+
+
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
---
+++ caodocao/banco/racas de gatos.sql
@@ -0,0 +1,59 @@
+
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Abissínio');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Angorá turco' );
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'American curl');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'American Shorthair');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'American wirehair');   
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'American wirehair');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Azul rusoan');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Balinês');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Bengal');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Bobtail Japonês');    
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Bombay');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Bosque da Noruega / Norwegian Forest');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'British Shorthair');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Burmês');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Burmês Europeu');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'California spangled');    
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Chartreux');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Cornish rex');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Devon Rex');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Europeu Comum / Common European');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Exótico');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Habana');   
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Himalayan');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Indefinido / Undefined');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Javanês');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Korat');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Maine coon');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Manx');    
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Mau Egípcio');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Nibelungo');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Ocicat');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Oriental de pêlo curto');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Persa / Persian');   
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Ragamuffin');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Ragdoll');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Sagrado da Birmânia');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Scottish fold');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Selkirk rex'); 
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Siamês / Siamese');     
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Singapur');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Shorthair Americano');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Shorthair Britânico');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Shorthair Exótico');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Snowshoe');    
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Somali');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Sphynx');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Tiffanie');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Tonquinês');
+INSERT INTO `raca` ( `cod_especie`, `descricao`) VALUES (2 , 'Van turco');
+
+
+
+	 
+
+
+
+ 
+	 
