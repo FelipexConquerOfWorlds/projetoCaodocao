60ed6e89fda6e697129b1126164213bf70bf54e0
FelipexConquerOfWorlds <felipepborba@hotmail.com>
2018-10-23 09:15:55 -0300

    Add files via upload

--- caodocao/models/Doacao.php
+++ caodocao/models/Doacao.php
@@ -8,13 +8,12 @@ class Doacao
     private $cod_status;
     private $cod_usu;
 
-        public function __construct($data_doacao, $data_cadastro, $cod_usu, $cod_status, $cod_doacao)
+        public function __construct($cod_usu, $cod_status, $data_cadastro = null, $data_doacao = null)
         {
             $this->data_cadastro = $data_cadastro;
             $this->data_doacao = $data_doacao;
             $this->cod_status = $cod_status;
             $this->cod_usu = $cod_usu;
-            $this->cod_doacao = $cod_doacao;
         }
 
     /**
