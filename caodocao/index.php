d56f34c671877661d56202ce152a5da7fe06eab5
FelipexConquerOfWorlds <felipepborba@hotmail.com>
2018-11-30 15:15:11 -0200

    Add files via upload

--- caodocao/index.php
+++ caodocao/index.php
@@ -5,7 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-  <link rel="stylesheet" href="https://v40.pingendo.com/assets/4.0.0/default/theme.css" type="text/css"> </head>
+  <link rel="stylesheet" href="https://v40.pingendo.com/assets/4.0.0/default/theme.css" type="text/css">
+</head>
 
 <body>
   <nav class="navbar navbar-expand-md navbar-light bg-dark">
@@ -13,31 +14,30 @@
       <a class="navbar-brand" href="">
         <img src="views/img/logo.png" class="d-inline-block align-top rounded" alt="" width="250" height="60"> </a>
       <a class="navbar-brand" href="#">
-        <b href="index.html" style="background-image: url('../../projetoCaodocao-master/caodocao/assets/img/logo.png');background-position:left center;background-repeat:repeat;" class="w-75"> </b>
       </a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item"> </li>
-        </ul>
-        <a class="btn navbar-btn ml-2 text-dark btn-warning" href="controller/sfdk.php?acao=login">
+        </ul><div>
+        <a class="btn navbar-btn ml-2 text-dark btn-warning"  href="controller/sfdk.php?acao=login">
           <i class="fa d-inline fa-lg fa-user-circle-o"></i> Login</a>
         <a class="btn navbar-btn ml-2 text-dark btn-warning" href="views/cadastro.html">
           <i class="fa d-inline fa-lg fa-sign-in"></i> Cadastrar-se</a>
+        </div>
       </div>
     </div>
   </nav>
-  <div class="py-5 text-center" style="background-image: url('views/img/portfolio/placeholder-2.png');background-size:cover;">
+  <div class="py-5 text-center" style="background-image: url('views/img/portfolio/placeholder-2.png');background-size:100%;position relative;display: block">
     <div class="container py-5">
       <div class="row">
         <div class="col-md-12">
           <h1 class="display-3 mb-4 text-black">Cãodoção</h1>
-            <br>
-            <br>
-            <br>
-          <a href="controller/sfdklogado.php?acao=adotar" class="btn btn-lg mx-1 btn-warning text-dark">Adotar</a>
+          <br>
+          <br>
+          <br>
+          <a href="controller/sfdk.php?acao=login" class="btn btn-lg mx-1 btn-warning text-dark">Adotar</a>
           <a href="controller/usuarios.php?acao=deslogado" class="btn btn-lg btn-warning mx-1 text-dark">Doar</a>
         </div>
       </div>
@@ -73,24 +73,21 @@
     </div>
   </div>
   <div class=" bg-warning">
-
     <div class="container bg-dark">
       <div class="row mb-5">
         <div class="col-md-6 bg-dark       offset-md-1" style="transition: all 0.25s;">
           <h2 class="text-warning">Article subtitle #1</h2>
-          <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class="col-md-5 align-self-center bg-dark" style="transition: all 0.25s;">
-          <img class="img-fluid d-block w-100 img-thumbnail p-2 bg-dark border-dark" src="views/img/portfolio/REINOUNIDO_serialkiller_gatos_pexels-126407.jpg"> </div>
+          <img class="img-fluid d-block w-100 img-thumbnail p-2 bg-dark border-dark" src="views/img/portfolio/salvando.jpg"> </div>
       </div>
       <div class="row">
         <div class="col-md-5">
           <img class="img-fluid d-block mb-4 w-100 img-thumbnail bg-dark p-2 border-dark" src="views/img/portfolio/placeholder-12.png"> </div>
         <div class="col-md-5">
           <h2 class="pt-3 text-warning">Article subtitle #2</h2>
-          <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
     </div>
@@ -98,90 +95,9 @@
   <div class=" bg-warning">
     <div class="container">
       <div class="row text-center">
-        <div class="col-md-12 bg-dark">
+        <div class="col-md-12 bg-dark" style="">
           <h1 class="mb-4 text-warning">Benefits and features</h1>
-          <p class="lead">Design unique interfaces by customizing Bootstrap theme with Pingendo.
-            <br>Modify colors, fonts and grid setting without opening any CSS file.</p>
-          <div class="row text-left mt-5">
-            <div class="col-md-4 my-3">
-              <div class="row mb-3">
-                <div class="text-center col-2">
-                  <i class="d-block mx-auto fa fa-3x fa-mars"></i>
-                </div>
-                <div class="align-self-center col-10">
-                  <h5 class="text-warning">
-                    <b>For him</b>
-                  </h5>
-                </div>
-              </div>
-              <p>Based on fluid design principles. Works with screen resolution. &nbsp;In-app chat panel 24/7 active. The support you need, right there.</p>
-            </div>
-            <div class="col-md-4 my-3">
-              <div class="row mb-3">
-                <div class="text-center col-2">
-                  <i class="d-block mx-auto fa fa-3x fa-venus"></i>
-                </div>
-                <div class="align-self-center col-10">
-                  <h5 class="text-warning">
-                    <b>For her</b>
-                  </h5>
-                </div>
-              </div>
-              <p>Help us spreading the word. Tell your friends with just one-click.&nbsp;Work simultaneously on different panels with the switcher</p>
-            </div>
-            <div class="col-md-4 my-3">
-              <div class="row mb-3">
-                <div class="text-center col-2">
-                  <i class="d-block mx-auto fa fa-3x fa-neuter"></i>
-                </div>
-                <div class="align-self-center col-10">
-                  <h5 class="text-warning">
-                    <b>For its</b>
-                  </h5>
-                </div>
-              </div>
-              <p>You are working with plain HTML and SASS files on your computer, so extending and integrating Javascript is a breeze.</p>
-            </div>
-            <div class="col-md-4 my-3">
-              <div class="row mb-3">
-                <div class="text-center col-2">
-                  <i class="d-block mx-auto fa fa-3x fa-mars-stroke"></i>
-                </div>
-                <div class="align-self-center col-10">
-                  <h5 class="text-warning">
-                    <b>For anyone</b>
-                  </h5>
-                </div>
-              </div>
-              <p>Based on fluid design principles. Works with screen resolution.&nbsp;Choose settings depending on the criteria you value the most.</p>
-            </div>
-            <div class="col-md-4 my-3">
-              <div class="row mb-3">
-                <div class="text-center col-2">
-                  <i class="d-block mx-auto fa fa-3x fa-mars-double"></i>
-                </div>
-                <div class="align-self-center col-10">
-                  <h5 class="text-warning">
-                    <b>Even couples</b>
-                  </h5>
-                </div>
-              </div>
-              <p>In-app chat panel 24/7 active. The support you need, right there. Pingendo enhance the battery duration and the quality of your life.&nbsp;</p>
-            </div>
-            <div class="col-md-4 my-3">
-              <div class="row mb-3">
-                <div class="text-center col-2">
-                  <i class="d-block mx-auto fa fa-3x fa-genderless"></i>
-                </div>
-                <div class="align-self-center col-10">
-                  <h5 class="text-warning">
-                    <b>Any other?</b>
-                  </h5>
-                </div>
-              </div>
-              <p>Help us spreading the word. Tell your friends with just one-click.&nbsp;Work simultaneously on different panels. Share the work with teammates.</p>
-            </div>
-          </div>
+          <p class="lead">Design unique interfaces by customizing Bootstrap theme with Pingendo. <br>Modify colors, fonts and grid setting without opening any CSS file.</p>
         </div>
       </div>
     </div>
@@ -206,7 +122,6 @@
       </div>
     </div>
   </div>
-
 </body>
 
 </html>
